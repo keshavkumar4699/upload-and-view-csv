a1928a3d859c15588006bb9b6703f3a9bbb3a9da
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-05 16:13:49 +0530

    jquery added

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,12 @@
-function deleteCSV(e){
-  let url =  `/destroy?id=${e.getAttribute('data-id')}`;
+function deleteCSV(e) {
+  let url = `/destroy?id=${e.getAttribute("data-id")}`;
   window.get(url);
 }
 
-window.get = function(url, data) {
-  return fetch(url, {method: "GET", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
-}
-
+window.get = function (url, data) {
+  return fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+};

c796a3591806cc771b684e9beb0aac6311bb966f
Keshav kumar <kumar.keshav4699@gmail.com>
2023-10-09 17:02:37 +0530

    added mongodb cluster link

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,14 @@
 //require library
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //connect to database
 async function main() {
-    const db = await mongoose.connect(`mongodb://127.0.0.1:27017/csv-upload`);
-    module.exports = db;
+  const db = await mongoose.connect(
+    `mongodb+srv://kumarkeshav4699:ORpSNGz3ZpVYspah@beginnercluster.k6gk8p2.mongodb.net/?retryWrites=true`
+  );
+  module.exports = db;
 }
 
 main()
-.then(() => console.log('database connected')) //if connected
-.catch(err => console.log(err)); //if error
+  .then(() => console.log("database connected")) //if connected
+  .catch((err) => console.log(err)); //if error
